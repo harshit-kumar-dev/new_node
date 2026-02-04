d44617d3245d3f05d4670a07e7b1abba6b433ffe
Harshit kumar <harshit.kumar.cg@gmail.com>
2026-02-04 17:18:20 +0530

    added comments

--- index.js
+++ index.js
@@ -12,31 +12,34 @@ app.get("/", (req, res) => {
   res.send("Hi, my name is Harshit kumar");
 });
 
-app.get("/route2/:one", (req, res) => {
-  res.send("Express server is running here in route 2");
-  console.log("first");
-});
+
+// this first will always run and it will never run the sescond.  (as EXPRESS checks from top to bottom).
+  app.get("/route2/:one", (req, res) => {
+    res.send("Express server is running here in route 2");
+    console.log("first");
+  });
+
+  app.get("/route2/1", (req, res) => {
+      res.status(200).json(users);
+      console.log("second");
+  });
 
 
-app.get("/users/1", (req, res) => {
-    res.status(200).json(users);
-    console.log("second");
-});
+
+// check the id and give the result
+  app.get("/users/:id", (req, res) => {
+    const userId = Number(req.params.id);
+    const user = users.find(u => u.id === userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  });
 
 
-
-app.get("/users/:id", (req, res) => {
-  const userId = Number(req.params.id);
-  const user = users.find(u => u.id === userId);
-
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
-  }
-
-  res.status(200).json(user);
-});
-
-
-app.listen(3000, () => {                                  /* Always comes at last */
-  console.log("Server started on port 3000");
-});
+// Compulsory for the server.
+  app.listen(3000, () => {                                  /* Always prefered at last */
+    console.log("Server started on port 3000");
+  });
